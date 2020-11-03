7526bd6be4f6e3c5df95424858885df4fbfe449b
Alex Grobovsky <ktotaam@mail.ru>
2020-11-03 12:53:26 +0500

    added about section + adaptive

--- js/app.js
+++ js/app.js
@@ -1881,11 +1881,28 @@ for (let i = 0; i < favorites.length; i++) {
   favoriteBtn.addEventListener('click', function (e) {
     favoriteBtn.classList.toggle("added");
   });
-}
+} ///////
+/// PHONE MASK
+//////
+
 
 let selector = document.querySelectorAll('input[type="tel"]');
 let im = new Inputmask("+7 (999) 999-99-99");
-im.mask(selector);
+im.mask(selector); //////////////////
+/// Validation
+/////////////////
+
+let validateForms = function (selector, rules) {
+  new window.JustValidate(selector, {
+    rules: rules
+  });
+};
+
+validateForms('.call-request', {
+  tel: {
+    required: true
+  }
+});
 $(function () {
   $('.filter__select').select2({
     width: "100%",
