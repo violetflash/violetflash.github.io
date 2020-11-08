dd07e2f0742df1c2489e4e427fa0aab978794919
Alex Grobovsky <ktotaam@mail.ru>
2020-11-08 15:36:31 +0500

    fixed menu behavior fix

--- js/app.js
+++ js/app.js
@@ -1894,7 +1894,76 @@ function ibg() {
   }
 }
 
-ibg(); // TABS
+ibg(); //=================================================================
+//SlideToggle
+
+let _slideUp = (target, duration = 500) => {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = target.offsetHeight + 'px';
+  target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout(() => {
+    target.style.display = 'none';
+    target.style.removeProperty('height');
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+    target.classList.remove('_slide');
+  }, duration);
+};
+
+let _slideDown = (target, duration = 500) => {
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+  if (display === 'none') display = 'block';
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout(() => {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+    target.classList.remove('_slide');
+  }, duration);
+};
+
+let _slideToggle = (target, duration = 500) => {
+  if (!target.classList.contains('_slide')) {
+    target.classList.add('_slide');
+
+    if (window.getComputedStyle(target).display === 'none') {
+      return _slideDown(target, duration);
+    } else {
+      return _slideUp(target, duration);
+    }
+  }
+}; //=====================================================================
+// TABS
+
 
 let tabs = document.querySelectorAll('._tabs');
 
@@ -2088,17 +2157,29 @@ $(function () {
       $('.bottom-header__dropdown-list').slideUp(300);
       $('.bottom-header__dropdown-btn').removeClass('_active');
     }
-  });
-  $(".scroll-header").hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 10) {
-      $('.scroll-header').slideDown(500);
-      $('.header').slideUp(500);
-    } else {
-      $('.scroll-header').slideUp(500);
-      $('.header').slideDown(500);
+  }); //fixed header
+
+  window.addEventListener('scroll', function (e) {
+    let header = document.querySelector('.header');
+    let headerBottom = header.offsetTop + header.offsetHeight;
+    let headerScroll = document.querySelector('.scroll-header');
+
+    if (window.scrollY >= headerBottom) {
+      headerScroll.className += ' visible';
+    } else if (window.scrollY < headerBottom) {
+      headerScroll.classList.remove('visible');
     }
-  }); ///MODAL LOGIN-REGISTER
+  }); // $(".scroll-header").hide();
+  // $(window).scroll(function() {
+  //     if ($(this).scrollTop() > 10) {
+  //         $('.scroll-header').slideDown(500);
+  //         $('.header').slideUp(500);
+  //     } else {
+  //         $('.scroll-header').slideUp(500);
+  //         $('.header').slideDown(500);
+  //     }
+  // });
+  ///MODAL LOGIN-REGISTER
 
   $('.modal-login').magnificPopup({
     type: 'inline',
