451362a5c5e56e982fb801a2bf2d91d0cd1ce983
Alex Grobovsky <ktotaam@mail.ru>
2020-11-14 00:09:51 +0500

    horizontal scroll fix

--- energy-m/js/app.js
+++ energy-m/js/app.js
@@ -648,18 +648,15 @@ dropdownBtn.addEventListener('mouseleave', function (e) {
 const myModal = new HystModal({
   linkAttributeName: "data-hystmodal" // настройки (не обязательно), см. API
 
-});
+}); //Поле ввода времени звонка становится активным после активации нужной радиокнопки
+
 let timeCall = document.querySelector('.js-time');
-let timeCall_msg = document.querySelector('.request__time');
+let timeCall_msg = document.querySelector('.request__time .request__textarea');
 let checkbox_time = document.querySelectorAll('.request__options .checkbox__input');
 
 for (let i = 0; i < checkbox_time.length; i++) {
   checkbox_time[i].addEventListener('click', function (e) {
-    if (timeCall.checked) {
-      timeCall_msg.classList.add('js-visible');
-    } else {
-      timeCall_msg.classList.remove('js-visible');
-    }
+    timeCall_msg.disabled = !timeCall.checked;
   });
 }
 
